e7a62643be7a9b6a31775718dfef6056fd3da904
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-07-22 12:26:36 -0600

    more js logic including searching, fetching, displaying movies

--- Movie App/client/script.js
+++ Movie App/client/script.js
@@ -2,18 +2,31 @@
 const API_PATH =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=578421658349dbe13bbe06fe76a6eaa1&page=1";
 const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280";
-const QUERY_PATH = `https://api.themoviedb.org/3/search/movie?&api_key=578421658349dbe13bbe06fe76a6eaa1&query=Sam`;
+let QUERY_PATH = `https://api.themoviedb.org/3/search/movie?&api_key=578421658349dbe13bbe06fe76a6eaa1&query=`;
 
 const searchForm = document.getElementById("search-form");
 const searchBar = document.getElementById("search-input");
+const moviesColumn = document.getElementsByClassName("column-container");
+const moviesRow = document.getElementsByClassName("row-container");
+
+searchBar.onclick = () => {
+  searchBar.placeholder = "";
+};
 
 searchForm.onsubmit = searchMovie;
 
+//when user searches up a movie
 function searchMovie(e) {
-  e.preventDefault(); //disable unwanted form reload
+  e.preventDefault();
   fetchMovies(searchBar.value);
+  displayMovies();
 }
 
+//fetches the movie(s) from API given the searched movie
 async function fetchMovies(movie) {
-  const fetchedMoviesRes = await fetch();
+  QUERY_PATH += `${movie}`;
+  const fetchedMoviesRes = await fetch(QUERY_PATH);
+  console.log(await fetchedMoviesRes.json());
 }
+
+function displayMovies() {}
