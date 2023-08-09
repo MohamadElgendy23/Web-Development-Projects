7ca32eba568fc407a154cba4c0b1666ed366a9f6
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-08-09 15:19:51 -0600

    connecting db

--- MyLibrary/server.js
+++ MyLibrary/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
 
@@ -8,6 +9,15 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("DATABASE CONNECTED");
+  })
+  .catch(() => {
+    console.log("DATABASE NOT CONNECTED");
+  });
 
 app.use("/", indexRouter);
 
