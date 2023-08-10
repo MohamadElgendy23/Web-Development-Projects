f16e2dd30e468063fe833f35cd66d71b1391c467
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2023-08-10 16:44:01 -0600

    removed BookHouse

--- BookHouse/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-//renders out html using .ejs file on runtime
-router.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-
-module.exports = router;
